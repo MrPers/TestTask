b797e2502747bbaec75cb9df7aa1f4571a293acc
Anton Anton <iamanton45@gmail.com>
2023-02-20 10:32:15 +0000

    Add project files.

---
+++ Rickandmorty.Contracts/Services/IRickMortyService.cs
@@ -0,0 +1,17 @@
+﻿using Rickandmorty.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rickandmorty.Contracts.Services
+{
+    public interface IRickMortyService
+    {
+        Task<List<PersonFullInformationDto>> GetPersonsFullInformationAsync(string name);
+        Task<bool> GetCheckPersonAsync(CheckPersonDto letter);
+        Task<bool> SaveEpisodesAsync();
+        int GetEpisodes(string episodeName);
+    }
+}
---
+++ Rickandmorty.Data/DataSample.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Rickandmorty.Contracts.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rickandmorty.Logic
+{
+    public class DataSample
+    {
+        public static async Task InitializeAsync(IServiceScope serviceScope)
+        {
+
+            IServiceProvider scopeServiceProvider = serviceScope.ServiceProvider;
+            var _rickMortyService = scopeServiceProvider.GetRequiredService<IRickMortyService>();
+
+            await _rickMortyService.SaveEpisodesAsync();
+
+        }
+    }
+}
